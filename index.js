77cf14381b9f7ad43c2732bde85778b38a97327e
julietsanchez <112452557+julietsanchez@users.noreply.github.com>
2025-08-07 06:49:37 -0300

    1

---
+++ index.js
@@ -0,0 +1,13 @@
+//MODAL 
+const modal = document.getElementById('welcomeModal');
+      const music = document.getElementById('bgMusic');
+
+      function startWithMusic() {
+        modal.style.display = 'none';
+        music.play();
+      }
+
+      function startWithoutMusic() {
+        modal.style.display = 'none';
+      }
+//MUSICA 
